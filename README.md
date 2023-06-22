48a4c035027adc292fd2138532f50fe03e2ca610
h0lgrr <github@h0lgrr.de>
2023-06-22 15:48:49 +0200

    add update.sh

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # raspi-config
+
+## scripts
+
+```sh
+wget -O - https://xxx.yy/file.sh | bash
+```
---
+++ src/update.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sudo apt update
+sudo apt dist-upgrade
+sudo apt install build-essential git curl vim
